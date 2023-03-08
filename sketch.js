16ed7f0d6704bb792206075f899c3988ccc51b8a
Kevin <115562314+KeBin301@users.noreply.github.com>
2023-03-07 22:42:43 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let x=0;
+let x = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -6,21 +6,22 @@ function setup() {
 
 function draw() {
   background(220);
+
   strokeWeight(2);
   fill(200, 200, 200);
   rect(0, 350, width, 50);
-  
+
   //main body of the fridge
   fill("violet");
   rect(100, 100, 200, 250, 20);
- 
+
   // handle
   fill(150, 150, 150);
   rect(280, 200, 20, 80);
   ellipse(290, 240, 20, 20);
-  
+
   //divider
-  fill("purple");
+  fill("purple  ");
   rect(100, 175, 200, 15);
 
   //magnets
@@ -34,24 +35,24 @@ function draw() {
   ellipse(226, 254, 20, 20);
   ellipse(140, 220, 20, 20);
   ellipse(280, 311, 20, 20);
-  
+
   //mouth
   noFill();
-  arc(200,270,150,100,PI/8, 7*PI/8);
-  
+  arc(200, 270, 150, 100, PI / 8, (7 * PI) / 8);
+
   //eyes
-  if (x >= 0){
+  if (x >= 0) {
     fill(255);
     ellipse(150, 70, 80, 80);
     ellipse(250, 70, 80, 80);
     x--;
-    
   } else if (x <= 1) {
     fill(255);
     ellipse(150, 70, 80, 30);
     ellipse(250, 70, 80, 30);
     x++;
   }
+
   fill(0);
   ellipse(150, 70, 40, 40);
   ellipse(250, 70, 40, 40);
